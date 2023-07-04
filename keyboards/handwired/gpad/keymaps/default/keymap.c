aad96b94bd9887dffac8a38484f3872642e131a1
sadchi <sadchi@gmail.com>
2023-07-05 00:12:15 +0300

    Gpad added

---
+++ keyboards/handwired/gpad/keymaps/default/keymap.c
@@ -0,0 +1,13 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_GRAVE,   KC_1,   KC_2,   KC_3, KC_4, KC_T,
+        KC_TAB,   KC_Z,   KC_Q,   KC_W, KC_E, KC_R, KC_G, 
+        KC_LALT,   KC_X,   KC_A,   KC_S, KC_D,KC_F, KC_SPACE, KC_BSPC,
+        KC_B,   KC_RCTL, KC_N
+    ),
+};

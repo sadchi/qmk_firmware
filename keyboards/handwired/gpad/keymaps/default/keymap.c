c1adc3530517bca4404b53989cf4cf6ae23c7684
sadchi <sadchi@gmail.com>
2023-07-05 04:15:03 +0300

    Changed keymap

--- keyboards/handwired/gpad/keymaps/default/keymap.c
+++ keyboards/handwired/gpad/keymaps/default/keymap.c
@@ -5,9 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_GRAVE,   KC_1,   KC_2,   KC_3, KC_4, KC_T,
+        KC_ESC,   KC_1,   KC_2,   KC_3, KC_4, KC_T,
         KC_TAB,   KC_Z,   KC_Q,   KC_W, KC_E, KC_R, KC_G, 
         KC_LALT,   KC_X,   KC_A,   KC_S, KC_D,KC_F, KC_SPACE, KC_BSPC,
-        KC_B,   KC_RCTL, KC_N
+        KC_B, KC_N, KC_RCTL
     ),
 };
